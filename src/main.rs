b7943e3d37cdffcfe85002a8ef62feaa8740be4c
hs785 <125337327+Henilp7201@users.noreply.github.com>
2025-10-16 22:33:57 -0400

    added name feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter your name:");
+
+    let mut name = String::new(); 
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line"); 
+
+    let name = name.trim();
+
+    println!("Hello, {}!", name);
 }
